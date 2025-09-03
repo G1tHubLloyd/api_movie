9223eeb43a38e51f0f0ed7f152051718963718c1
G1tHubLloyd <jongielobo@rocketmail.com>
2025-09-03 13:23:02 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-const movieRoutes = require('./routes/movies');
-const userRoutes = require('./routes/users');
-
 const app = express();
+app.use(express.json());
 
-// Middleware
-app.use(bodyParser.json());
-app.use(cors());
+// Movies
+app.get('/movies', (req, res) => {
+  res.send('GET request: Returning all movies');
+});
 
-// MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/moviesDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+app.get('/movies/:title', (req, res) => {
+  res.send(`GET request: Returning data for movie titled "${req.params.title}"`);
+});
 
-// Routes
-app.use('/movies', movieRoutes);
-app.use('/users', userRoutes);
+// Genres
+app.get('/genres/:name', (req, res) => {
+  res.send(`GET request: Returning description for genre "${req.params.name}"`);
+});
 
-// Start Server
-const PORT = process.env.PORT || 3000;
+// Directors
+app.get('/directors/:name', (req, res) => {
+  res.send(`GET request: Returning bio for director "${req.params.name}"`);
+});
+
+// Users
+app.post('/users', (req, res) => {
+  res.send('POST request: Registering new user');
+});
+
+app.put('/users/:username', (req, res) => {
+  res.send(`PUT request: Updating user "${req.params.username}"`);
+});
+
+app.post('/users/:username/favorites/:movieTitle', (req, res) => {
+  res.send(`POST request: Adding movie "${req.params.movieTitle}" to favorites for user "${req.params.username}"`);
+});
+
+app.delete('/users/:username/favorites/:movieTitle', (req, res) => {
+  res.send(`DELETE request: Removing movie "${req.params.movieTitle}" from favorites for user "${req.params.username}"`);
+});
+
+app.delete('/users/:username', (req, res) => {
+  res.send(`DELETE request: Deregistering user "${req.params.username}"`);
+});
+
+// Start server
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
